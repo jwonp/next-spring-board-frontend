9bc812e600b868fffdbb3152b2aa413e4a96a41d
Joowon <201611804@kyonggi.ac.kr>
2023-04-13 23:58:47 +0900

    remove unused codes

--- src/pages/api/board/search/[target].ts
+++ src/pages/api/board/search/[target].ts
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { contentType } from "@src/static/types/contentType";
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 const searchByTarget = async (target: string) => {
--- src/pages/api/board/size.ts
+++ src/pages/api/board/size.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { contentType } from "@src/static/types/contentType";
+
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(
