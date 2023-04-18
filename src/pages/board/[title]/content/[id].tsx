e5fb381fd5af745545a8b4c9341dfac4fca9ce14
Joowon <201611804@kyonggi.ac.kr>
2023-04-18 23:28:26 +0900

    update content view

--- src/pages/board/[title]/content/[id].tsx
+++ src/pages/board/[title]/content/[id].tsx
@@ -1,12 +1,27 @@
 import styles from "@src/styles/board/content/ContentById.module.scss";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
-import { useMemo } from "react";
-const ContentById = () => {
+import { useEffect, useMemo } from "react";
+const ContentById = ({}: InferGetServerSidePropsType<
+  typeof getServerSideProps
+>) => {
   const router = useRouter();
-  const id = useMemo(() => {
-    return router.query.id;
-  }, [router.asPath]);
-  return <div>{id}</div>;
+  const { title, id } = router.query;
+
+  // const id = useMemo(() => {
+  //   return router.query.id;
+  // }, [router.asPath]);
+  return (
+    <div>
+      <div>{title}</div>
+      <div>{id}</div>
+    </div>
+  );
 };
 
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  return {
+    props: {},
+  };
+};
 export default ContentById;
