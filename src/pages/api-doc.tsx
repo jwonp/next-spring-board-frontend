5ac248f3f9d83763ab7d75fa81e2f55dbcc85226
Joowon <tkdel222@gmail.com>
2023-05-15 16:42:39 +0900

    update next swagger

---
+++ src/pages/api-doc.tsx
@@ -0,0 +1,38 @@
+import styles from "@src/styles/swagger.module.scss";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { createSwaggerSpec } from "next-swagger-doc";
+import dynamic from "next/dynamic";
+
+import "swagger-ui-react/swagger-ui.css";
+
+const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
+
+function ApiDoc({ spec }: InferGetStaticPropsType<typeof getStaticProps>) {
+  console.log(spec);
+  return (
+    <div className={`${styles.wrapper}`}>
+      <SwaggerUI spec={spec} />;
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const spec: Record<string, any> = createSwaggerSpec({
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Next Swagger API Example",
+        version: "1.0",
+      },
+    },
+    apiFolder: "src/pages/api",
+  });
+
+  return {
+    props: {
+      spec,
+    },
+  };
+};
+
+export default ApiDoc;
