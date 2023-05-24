c1952a6701ecc1a4e0142cce19404c0df32413ba
Joowon <tkdel222@gmail.com>
2023-05-24 17:46:34 +0900

    update next config js

--- next.config.js
+++ next.config.js
@@ -2,13 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["127.0.0.1", "127.0.0.1:8000"],
+    domains: ["127.0.0.1", "127.0.0.1:8000", process.env.IMAGES_PROMOTE_DOMAIN],
     remotePatterns: [
       {
         protocol: "http",
         hostname: "127.0.0.1",
         port: "8000",
         pathname: "/files/display/**/**",
+      },
+      {
+        protocol: "http",
+        hostname: process.env.IMAGES_PROMOTE_HOSTNAME,
+        port: process.env.IMAGES_PROMOTE_PORT,
+        pathname: process.env.IMAGES_PROMOTE_PATHNAME,
+      },
+      {
+        protocol: "https",
+        hostname: process.env.IMAGES_PROMOTE_HOSTNAME,
+        port: process.env.IMAGES_PROMOTE_PORT,
+        pathname: process.env.IMAGES_PROMOTE_PATHNAME,
       },
     ],
   },
